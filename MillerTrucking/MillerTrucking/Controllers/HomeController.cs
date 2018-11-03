e2b6ab82aa2e342ddf56aa5d14a1c2b958217d5d
Justin <justinrh97@gmail.com>
2018-11-03 12:07:06 -0400

    Removed start up code.

--- MillerTrucking/MillerTrucking/Controllers/HomeController.cs
+++ MillerTrucking/MillerTrucking/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MillerTrucking.Models;
 
 namespace MillerTrucking.Controllers
 {
@@ -13,31 +12,6 @@ namespace MillerTrucking.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
