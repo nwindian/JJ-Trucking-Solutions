be5498be519715f3ddb3e573c60ea49fb8d1ec76
Justin <justinrh97@gmail.com>
2018-11-17 22:13:17 -0500

    Working on accessing controller

--- MillerTrucking/MillerTrucking/Controllers/HomeController.cs
+++ MillerTrucking/MillerTrucking/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace MillerTrucking.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult FindTrucks()
         {
             throw new NotImplementedException();
